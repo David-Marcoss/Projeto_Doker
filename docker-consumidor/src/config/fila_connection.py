4660e7d2c6e0bc0b01503f4dc18e11fd9c5d065e
David Marcos <ddragondavi@gmail.com>
2023-06-17 14:35:09 -0300

    -corrigindo bugs

--- docker-consumidor/src/config/fila_connection.py
+++ docker-consumidor/src/config/fila_connection.py
@@ -1,15 +1,20 @@
 import pika
+import requests
+
+from src import enderecos_op
+
+
+
 
 #classe responsavel por fazer conexão com fila , recebe callback
 #que é a função responsavel por consulmir mensages da fila
 class RabbitmqConsumer:
-    def __init__(self, callback) -> None:
+    def __init__(self) -> None:
         self.__host = "localhost"
         self.__port = 5672
         self.__username = "guest"
         self.__password = "guest"
         self.__queue = "My_fila"
-        self.__callback = callback
         self.__channel = self.__create_channel()
 
     #cria conexão com a flia
@@ -35,7 +40,28 @@ class RabbitmqConsumer:
         )
 
         return channel
+
     
+    #função responsavel por receber msg da fila e processa-la
+    def __callback(self,ch, method, properties, body):
+
+        cep = str(body, 'utf-8')
+
+        
+        #fazendo conexão com api externa
+        req = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
+        endereco = req.json()
+
+        if endereco != {'erro': True}:
+            #inserindo os dados obtidos via api no bd
+            op = enderecos_op()
+            op.insert(endereco["cep"],endereco["logradouro"],endereco["bairro"],endereco["uf"])
+
+            return "Operação concluida !!"
+            
+        else:
+            return "Cep não encontrado!"
+        
     def start(self):
         print(f'Listen RabbitMQ on Port 5672')
         self.__channel.start_consuming()
