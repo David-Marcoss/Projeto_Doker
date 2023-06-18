c290f365a48d4b96e76dd816e6eecb648b91b9d6
David Marcos <ddragondavi@gmail.com>
2023-06-18 16:40:58 -0300

    -update codigo schema.sql

--- docker-consumidor/db/schema.sql
+++ docker-consumidor/db/schema.sql
@@ -1,13 +1,16 @@
-
 CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
 USE `mydb` ;
 
+-- -----------------------------------------------------
+-- Table `mydb`.`enderecos`
+-- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`enderecos` (
   `idenderecos` INT NOT NULL,
   `cep` VARCHAR(45) NOT NULL,
-  `rua` VARCHAR(45) NOT NULL,
-  `cidade` VARCHAR(45) NOT NULL,
-  `estado` VARCHAR(45) NOT NULL,
+  `rua` VARCHAR(45) NULL,
+  `bairro` VARCHAR(45) NULL,
+  `cidade` VARCHAR(45) NULL,
+  `estado` VARCHAR(45) NULL,
   PRIMARY KEY (`idenderecos`),
   UNIQUE INDEX `cep_UNIQUE` (`cep` ASC) VISIBLE)
 ENGINE = InnoDB;
