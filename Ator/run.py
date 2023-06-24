8aa1f84add6de38051efbd99fcc442401087f9de
David Marcos <ddragondavi@gmail.com>
2023-06-24 11:32:45 -0300

    Implementando ator

---
+++ Ator/run.py
@@ -0,0 +1,21 @@
+from  flask  import  Flask,request,render_template
+import requests
+
+app  =  Flask( __name__ )
+ 
+@app.route('/',methods=["POST","GET"])
+def get_data():
+    if request.method == "POST":
+        
+        cep = request.form.get('cep')
+
+        return  requests.post("http://172.17.0.5:5000",json={"cep":cep}).text
+    
+ 
+    return render_template("home.html")
+    
+    
+
+
+if  __name__  ==  '__main__' : 
+    app.run()
